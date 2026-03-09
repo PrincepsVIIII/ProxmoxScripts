af321c0c75f091ed60c33920ce9081a7f1a2ec58
Princep <105245221+Raid-Princep@users.noreply.github.com>
2026-03-09 16:57:07 -0400

    clean CTFs complete

--- CleanCTFs.sh
+++ CleanCTFs.sh
@@ -3,3 +3,10 @@ POOL_BASE="SysSecTeam"
 NAME="LinuxCTF"
 
 VMIDS=$(qm list | grep "LinuxCTF" | awk '{print $1}')
+
+for VMID in $VMIDS; do
+    echo "Stopping and destroying VM $VMID..."
+    qm stop $VMID
+    qm destroy $VMID --purge
+    echo "$VMID stopped and destroyed."
+done
