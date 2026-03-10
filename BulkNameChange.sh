f11d3d300dd9b82dff7b714ababd383ce4e21feb
Princeps <143760961+PrincepsVIIII@users.noreply.github.com>
2026-03-10 13:12:43 -0400

    Refactor BulkNameChange script for name updates
    Updated the script to change VM names based on provided old and new names, and added functionality to rename multiple VMs.

---
+++ BulkNameChange.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OldName=$1
+NewName=$2
+
+VMIDS=$(qm list | grep ${OldName} | awk '{print $1}')
+
+for VMID in VMIDS; do
+  echo "changing $VMID name to $NewName"
+  qm set "$VMID" --name "$NewName"
+done
