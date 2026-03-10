9d728550b0df973c433ffd2426742d64466754a5
Princeps <143760961+PrincepsVIIII@users.noreply.github.com>
2026-03-10 13:15:38 -0400

    Quote variable expansions in BulkNameChange.sh

--- BulkNameChange.sh
+++ BulkNameChange.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-OldName=$1
-NewName=$2
+OldName="$1"
+NewName="$2"
 
-VMIDS=$(qm list | grep ${OldName} | awk '{print $1}')
+VMIDS=$(qm list | grep "${OldName}" | awk '{print $1}')
 
 for VMID in $VMIDS; do
   echo "changing $VMID name to $NewName"
