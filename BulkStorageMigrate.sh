895afb34b6913e23b387f8c679f04cdcd6a8277c
Princeps <143760961+PrincepsVIIII@users.noreply.github.com>
2026-03-11 11:25:23 -0400

    Add BulkStorageMigrate.sh for VM storage migration

---
+++ BulkStorageMigrate.sh
@@ -0,0 +1,9 @@
+Name="$1"
+StorageTarget="$2" #cdr-iscsi4, cdr-iscsi3
+
+VMIDS=$(qm list | grep " ${Name} " | awk '{print $1}')
+
+for VMID in $VMIDS; do
+  echo "Moving $VMID storage to $NewName"
+  qm disk move "$VMID" "scsi0" --name "$NewName" --format qcow2 --delete True
+done
