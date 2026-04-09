4f401336452a070bcc230157e1d583b72a89795e
Princeps <143760961+PrincepsVIIII@users.noreply.github.com>
2026-04-09 18:18:19 -0400

    removed /n

--- BulkPoolChange.sh
+++ BulkPoolChange.sh
@@ -22,7 +22,7 @@ fi
 # VM lookup - Exact matches
 # -------------------------
 VMIDS=$(qm list | awk -v name="$VMName" '$2 == name {print $1}')
-echo "Found VMIDs:\n$VMIDS"
+echo "Found VMIDs: $VMIDS"
 
 if [[ -z "$VMIDS" ]]; then
   echo "No VMs found matching $VMName"
